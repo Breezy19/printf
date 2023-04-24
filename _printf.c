dfbc311ec39c9e4e03954595b06f055e8016161f
Akram Freij <freij.akram@gmail.com>
2023-04-24 12:14:14 -0700

    team work printf

--- _printf.c
+++ _printf.c
@@ -1,24 +1,5 @@
 #include "main.h"
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-
-{
-
-if (*buff_ind > 0)
-
-write(1, &buffer[0], *buff_ind);
-
-
-
-*buff_ind = 0;
-
-}
-
+void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
@@ -103,5 +84,27 @@ va_end(list);
 
 
 return (printed_chars);
+
+}
+
+
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+
+{
+
+if (*buff_ind > 0)
+
+write(1, &buffer[0], *buff_ind);
+
+
+
+*buff_ind = 0;
 
 }
